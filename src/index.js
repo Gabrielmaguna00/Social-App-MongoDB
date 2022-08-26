5be2612aabca03ad355b74634f68617512e16cac
Gabriel Maguna <gabimaguna@gmail.com>
2022-08-26 10:43:30 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const mongooseConnect = require("./database/callDB");
 
 dotenv.config();
@@ -14,7 +14,7 @@ const v1UsersRoute = require("./v1/routes/user");
 const v1PostsRoute = require("./v1/routes/post");
 //MIDLEWARES
 app.use(express.json());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 //ROUTES
 app.use("/api/v1/user", v1UsersRoute);
